513be826f8849933b1e44e705ddf82f48497b9ea
ruijie20 <ruijieh20@gmail.com>
2013-09-06 08:55:56 +0800

    fix the test

--- ValueInjecterTry/Person.cs
+++ ValueInjecterTry/Person.cs
@@ -9,13 +9,12 @@ namespace ValueInjecterTry
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public int Conuntry1 { get; set; }
-        public int Conuntry2 { get; set; }
+        public int Country1 { get; set; }
+        public int Country2 { get; set; }
     }
 
     public class PersonDto
     {
-
         public int Id { get; set; }
         public string Name { get; set; }
 
@@ -40,23 +39,26 @@ namespace ValueInjecterTry
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != this.GetType()) return false;
             PersonDto other = (PersonDto) obj;
-            return Id == other.Id && string.Equals(Name, other.Name) && Country1 == other.Country1 && Country2 == other.Country2;
+            return Id == other.Id && string.Equals(Name, other.Name)
+                   && Country1 == other.Country1 && Country2 == other.Country2;
         }
     }
 
     public enum Country
     {
-        China, US, UK
+        China = 1,
+        US = 2,
+        UK = 3
     }
 
     public class IntToEnum : ConventionInjection
     {
         protected override bool Match(ConventionInfo c)
         {
-            return c.SourceProp.Name == c.TargetProp.Name && c.SourceProp.Type.IsSubclassOf(typeof (Enum))
-                   && c.TargetProp.Type == typeof (int);
+            return c.SourceProp.Type == typeof (int) && c.TargetProp.Type.IsSubclassOf(typeof (Enum))
+                   && c.SourceProp.Name == c.TargetProp.Name;
         }
-    } 
+    }
 
     [TestFixture]
     public class PersonTest
@@ -66,20 +68,28 @@ namespace ValueInjecterTry
         {
             var person = new Person
                 {
-                    Id = 1, Conuntry1 = 2, Conuntry2 = 1, Name = "abc"
+                    Id = 1,
+                    Country1 = 2,
+                    Country2 = 1,
+                    Name = "abc"
                 };
             var expectedPersonDto = new PersonDto
                 {
-                    Id = 1, Country1 = Country.US, Country2 = Country.China, Name = "abc"
+                    Id = 1,
+                    Country1 = Country.US,
+                    Country2 = Country.China,
+                    Name = "abc"
                 };
+
             var personDto = new PersonDto();
             personDto.InjectFrom<IntToEnum>(person)
-                .InjectFrom(person);
+                     .InjectFrom(person);
+
             Console.WriteLine(personDto.Country1);
             Console.WriteLine(personDto.Country2);
             Console.WriteLine(personDto.Name);
             Console.WriteLine(personDto.Id);
             Assert.That(personDto, Is.EqualTo(expectedPersonDto));
         }
-    } 
+    }
 }
